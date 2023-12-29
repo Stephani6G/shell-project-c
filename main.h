d9ea33ca2ef9f32bb21e4b0b0c2c333f62a76408
Stephani6G <stephaniegloria283@gmail.com>
2023-12-29 11:44:58 -0800

    main.h

--- main.h
+++ main.h
@@ -16,8 +16,40 @@
 #include <stddef.h>
 #include <sys/stat.h>
 
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+
 /* fizzbuzz */
 int main(void);
 int _putchar(char c);
+
+/**
+ * struct builtin_strruc - Builtin struct for command arguments
+ * @typen:  name of the command builtin
+ * @funcs: data type pointer function.
+ */
+
+typedef struct builtin_strruc
+{
+	char *typen;
+	int (*funcs)(datashel *dtsh);
+} builtin_typed;
+
+
+/**
+ * struct list_singly - singly linked list
+ * @lineui: command line interface
+ * @next: next node in a singly linked lists
+ * Description: single linked list to store command lines
+ */
+typedef struct line_list_singly
+{
+	char *lineui;
+	struct line_list_singly *next;
+} line_l;
+
+
 
 #endif
