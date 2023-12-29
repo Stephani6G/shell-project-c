ee9e266623811df60bce621541d7938c0e7f062c
Stephani6G <stephaniegloria283@gmail.com>
2023-12-29 13:23:06 -0800

    removed '_' in fucncs

--- err_1.c
+++ err_1.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * strcat_cd - function that concatenates the message for
@@ -13,28 +16,28 @@ char *strcat_cd(datashel *dtsh, char *text, char *errorm, char *vr_str)
 {
 	char *wrong_flag;
 
-	_strcpy(errorm, dtsh->av[0]);
-	_strcat(errorm, ": ");
-	_strcat(errorm, vr_str);
-	_strcat(errorm, ": ");
-	_strcat(errorm, dtsh->args[0]);
-	_strcat(errorm, text);
+	strcpy(errorm, dtsh->av[0]);
+	strcat(errorm, ": ");
+	strcat(errorm, vr_str);
+	strcat(errorm, ": ");
+	strcat(errorm, dtsh->args[0]);
+	strcat(errorm, text);
 	if (dtsh->args[1][0] == '-')
 	{
 		wrong_flag = malloc(3);
 		wrong_flag[0] = '-';
 		wrong_flag[1] = dtsh->args[1][1];
 		wrong_flag[2] = '\0';
-		_strcat(errorm, wrong_flag);
+		strcat(errorm, wrong_flag);
 		free(wrong_flag);
 	}
 
 	else
 	{
-		_strcat(errorm, dtsh->args[1]);
+		strcat(errorm, dtsh->args[1]);
 	}
 
-	_strcat(errorm, "\n");
-	_strcat(errorm, "\0");
+	strcat(errorm, "\n");
+	strcat(errorm, "\0");
 	return (errorm);
 }
