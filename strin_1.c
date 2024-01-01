5f19d2ee340e133886aa877e91cc7d90f0bf4e96
Stephani6G <stephaniegloria283@gmail.com>
2024-01-01 06:23:04 -0800

    strin_1.c

--- strin_1.c
+++ strin_1.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * strcat - a function that concatenate two strings
+ * _str_cat - a function that concatenate two strings
  * @dest: char pointer the dest of the copied str
  * @src: const char pointer the source of str
  * Return: the dest
  */
-char strcat(char *dest, const char *src)
+char _str_cat(char *dest, const char *src)
 {
 	int index;
 	int j;
@@ -24,13 +24,13 @@ char strcat(char *dest, const char *src)
 	return (dest);
 }
 /**
- * strcpy - function that  copies the
+ * _str_cpy - function that  copies the
  * string pointed to by src
  * @dest: Type char pointer the dest of the copied str
  * @src: Type char pointer the source of str
  * Return: the char
  */
-char strcpy(char *dest, char *src)
+char _str_cpy(char *dest, char *src)
 {
 
 	size_t ap;
@@ -44,13 +44,13 @@ char strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
- * strcmp - Function that compares two strings
+ * _str_cmp - Function that compares two strings
  * @s1: first typestring to be compared
  * @s2: second type string to be compared
  * Return: successs
  */
 
-int strcmp(char *s1, char *s2)
+int _str_cmp(char *s1, char *s2)
 {
 	int index;
 
@@ -64,12 +64,12 @@ int strcmp(char *s1, char *s2)
 	return (0);
 }
 /**
- * strchr - function that locates a character in a strin
+ * _str_chr - function that locates a character in a string
  * @str: the string
  * @c: character
  * Return: the pointer to the first occurrence of the character c
  */
-char strchr(char *str, char c)
+char _str_chr(char *str, char c)
 {
 	unsigned int index = 0;
 
@@ -81,12 +81,12 @@ char strchr(char *str, char c)
 	return ('\0');
 }
 /**
- * strspn - gets the length of a prefix substring
+ * _str_spn - gets the length of a prefix substring
  * @str: initial segment
  * @accept: accepted bytes
  * Return: the number of accepted bytes
  */
-int strspn(char *str, char *accept)
+int _str_spn(char *str, char *accept)
 {
 	int index, j, bool;
 
