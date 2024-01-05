935e1b2642d9bab0ea830aa2dbda9e3c1d5bd0a9
Stephani6G <stephaniegloria283@gmail.com>
2024-01-05 02:12:24 -0800

    l2st.c

---
+++ l2st.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * free_sep_list - frees a sep_list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_sep_list(sep_l **head)
+{
+	sep_l *temp;
+	sep_l *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * add_line_node_end - adds a command line at the end
+ * of a line_list
+ * @head: head of the linked list
+ * @line: command line
+ * Return: address of the head
+ */
+line_l *add_line_node_end(line_l **head, char *line)
+{
+	line_l *new, *temp;
+
+	new = malloc(sizeof(line_l));
+	if (new == NULL)
+		return (NULL);
+
+	new->line = line;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}
+
+/**
+ * free_line_list - frees a line_list
+ * @head: head of the linked list
+ */
+void free_line_list(line_l **head)
+{
+	line_l *temp;
+	line_l *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
