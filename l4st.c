ffaa3dc209008c49110f3041d9d6de7dc9883501
Stephani6G <stephaniegloria283@gmail.com>
2024-01-06 09:38:26 -0800

    l4st.c

--- l4st.c
+++ l4st.c
@@ -4,10 +4,10 @@
  * free_rvar_list - frees a r_var list
  * @head: head of the linked list.
  */
-void free_rvar_list(r_var **head)
+void free_rvar_list(r_vars **head)
 {
-	r_var *temp;
-	r_var *curr;
+	r_vars *temp;
+	r_vars *curr;
 
 	if (head != NULL)
 	{
