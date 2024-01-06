d41b3adae42bf5ac92c4153d6fa412974200b63e
Stephani6G <stephaniegloria283@gmail.com>
2024-01-06 10:16:51 -0800

    varenv.c

--- varenv.c
+++ varenv.c
@@ -38,3 +38,43 @@ void check_env(r_vars **h, char *in, datashel *data)
 
 	add_var_nodes(h, j, NULL, 0);
 }
+/**
+ * check_vars - function to check if the typed variable is $$ or $?
+ * @h: head of the linked list
+ * @in: input string
+ * @st: last status of the Shell
+ * @data: data structure
+ * Return: void success always
+ */
+int check_vars(r_vars **h, char *in, char *st, datashel *data)
+{
+	int i, lst, lpd;
+
+	lst = _str_len(st);
+	lpd = _str_len(data->pid);
+
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '$')
+		{
+			if (in[i + 1] == '?')
+				add_var_nodes(h, 2, st, lst), i++;
+			else if (in[i + 1] == '$')
+				add_var_nodes(h, 2, data->pid, lpd), i++;
+			else if (in[i + 1] == '\n')
+				add_var_nodes(h, 0, NULL, 0);
+			else if (in[i + 1] == '\0')
+				add_var_nodes(h, 0, NULL, 0);
+			else if (in[i + 1] == ' ')
+				add_var_nodes(h, 0, NULL, 0);
+			else if (in[i + 1] == '\t')
+				add_var_nodes(h, 0, NULL, 0);
+			else if (in[i + 1] == ';')
+				add_var_nodes(h, 0, NULL, 0);
+			else
+				check_env(h, in + i, data);
+		}
+	}
+
+	return (i);
+}
