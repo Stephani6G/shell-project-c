4beb16e72ef4c9625c509b9a0d7dc6cca0d07c0d
Stephani6G <stephaniegloria283@gmail.com>
2024-01-06 11:30:44 -0800

    readlin.c

--- readlin.c
+++ readlin.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * readlink - reads the input string
+ * readlin - reads the input string
  * @i_eof: return value of getline function
  * Return: input string
  */
-char *readlink(int *i_eof)
+char *readlin(int *i_eof)
 {
 	char *inputs = NULL;
 	size_t bufsize = 0;
