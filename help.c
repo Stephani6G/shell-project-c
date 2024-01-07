99caa6a08adb0119ed7ee6d1b4d12d1d0829f184
Stephani6G <stephaniegloria283@gmail.com>
2024-01-07 06:50:47 -0800

    help.c

--- help.c
+++ help.c
@@ -10,21 +10,21 @@ int get_help(datashel *dtsh)
 	char *data = dtsh->args[1];
 
 	if (data == 0)
-		aux_help_general();
+		help_general();
 	else if (_str_cmp(data, "setenv") == 0)
-		aux_help_setenv();
+		help_setenv();
 	else if (_str_cmp(data, "env") == 0)
-		aux_help_env();
+		help_environ();
 	else if (_str_cmp(data, "unsetenv") == 0)
-		aux_help_unsetenv();
+		help_unsetenv();
 	else if (_str_cmp(data, "help") == 0)
-		aux_help();
+		help_b();
 	else if (_str_cmp(data, "exit") == 0)
-		aux_help_exit();
+		help_exit();
 	else if (_str_cmp(data, "cd") == 0)
-		aux_help_cd();
+		help_cd();
 	else if (_str_cmp(data, "alias") == 0)
-		aux_help_alias();
+		help_alias();
 	else
 		write(STDERR_FILENO, dtsh->args[0], _str_len(dtsh->args[0]));
 
