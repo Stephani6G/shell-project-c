dfcec20dd5a56b9ad32b3102b380f3a68e01beaf
Stephani6G <stephaniegloria283@gmail.com>
2024-01-08 10:18:54 -0800

    exec_l.c

---
+++ exec_l.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * exec_line - finds builtins and commands
+ * @dtsh: relevant data
+ * Return: 1 on success
+ */
+int exec_line(datashel *dtsh)
+{
+	int (*builtin)(datashel *dtsh);
+
+	if (dtsh->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(dtsh->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(dtsh));
+
+	return (cmd_exec(dtsh));
+}
